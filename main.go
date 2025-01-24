4b533039166f660daaadb71d6aac09fcc277d97a
Admin <->
2025-01-24 19:03:43 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"net/http"
+)
+
+func helloFunc(w http.ResponseWriter, req *http.Request){
+	w.Write([]byte("Hello, world!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	http.HandleFunc("/", helloFunc)
+	http.ListenAndServe("localhost:80", mux)
+}
