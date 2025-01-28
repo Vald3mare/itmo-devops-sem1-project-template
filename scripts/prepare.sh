813e91eb209093f0610418189c72055d7e4dfae5
Admin <->
2025-01-28 15:51:38 +0300

    Pre-pre-pre-final

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+go mod tidy
+
+# Подготовка базы данных
+psql -U validator -d project-sem-1 -c "CREATE TABLE IF NOT EXISTS prices (
+    id SERIAL PRIMARY KEY,
+    product_id INT,
+    created_at DATE,
+    product_name TEXT,
+    category TEXT,
+    price DECIMAL(10, 2)
+);"
