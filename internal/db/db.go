d3706da5f9103a98919710f882f1fd7c5b4643a7
Admin <->
2025-02-28 21:12:22 +0300

    try6

--- internal/db/db.go
+++ internal/db/db.go
@@ -9,12 +9,12 @@ import (
 
 func InitDB() (*pgxpool.Pool, error) {
 	connString := fmt.Sprintf(
-		"user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	pool, err := pgxpool.New(context.Background(), connString)
