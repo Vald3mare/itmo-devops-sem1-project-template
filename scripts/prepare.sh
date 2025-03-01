f4ee410f8cc07b66fed8bc7a477bb8cac19668a2
Admin <->
2025-03-01 16:01:59 +0300

    fix-err_v0.5.1

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,16 +3,19 @@ set -e
 
 go mod download
 
-psql -U validator -d project-sem-1 -h localhost -p 5432 <<-EOSQL
+export PGPASSWORD="val1dat0r"
+
+# Команды для PostgreSQL (пароль будет взят из переменной окружения)
+psql -h localhost -p 5432 -U validator -d project-sem-1 <<-EOSQL
 DROP TABLE IF EXISTS prices;
-CREATE TABLE IF NOT EXISTS prices (
+CREATE TABLE prices (
     product_id TEXT,
     creation_date DATE,
     product_name TEXT,
     category TEXT,
     price NUMERIC
 );
-
 GRANT ALL PRIVILEGES ON TABLE prices TO validator;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO validator;
 EOSQL
+
+echo "Database setup completed successfully!"
