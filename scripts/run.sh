2d98e1717f6d4457620b26ac5b7abf739f374eb5
Admin <->
2025-03-01 16:07:51 +0300

    fix-err_v0.5.2

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-go build -o api cmd/api/main.go
-./api
+
+echo "Building the application..."
+cd сmd
+cd api
+go mod tidy
+go build -o api main.go
+
+
+echo "Starting the application in background..."
+nohup ./app > app.log 2>&1 &
+
+echo "Application started successfully in background."
+echo "Logs are being written to app.log"
